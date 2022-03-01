815b8ec29dba1e03e370f9a3757e5d1b9ba82a20
MANUEL ENRIQUE MORALES CHAVES <manuel.moraleschaves@ucr.ac.cr>
2022-03-01 00:55:23 -0600

    Mejoras generales: Session State para cargar solo una vez las capas WFS y barra de progreso inicial

--- main.py
+++ main.py
@@ -7,11 +7,8 @@
 
 import requests
 
-import math
-
 import streamlit as st
 
-import pandas as pd
 import geopandas as gpd
 
 import plotly.express as px
@@ -39,6 +36,7 @@ st.markdown('* Límite cantonal 1:5000')
 st.markdown('* Red vial 1:200000')
 st.markdown('El usuario debe seleccionar la categoría de vía que desee y la aplicación mostrará un conjunto de tablas, gráficos y mapas correspondientes a la longitud y la densidad de la red vial para la **categoría seleccionada**.')
 st.markdown('El código fuente de esta aplicación se encuentra alojado en el siguiente repositorio de [GitHub](https://github.com/manuel-moraleschaves/PF3311-proyecto).')
+st.markdown('')
 
 
 
@@ -46,65 +44,92 @@ st.markdown('El código fuente de esta aplicación se encuentra alojado en el si
 # 1. ENTRADAS
 #############
 
-#
-# Capa Límite cantonal 1:5000
-#
+# Validación para cargar los datos de las capas WFS únicamente una vez al iniciar la aplicación
+if 'datos_cargados' not in st.session_state:
 
-# Solicitud de capa WFS del límite cantonal mediante GET, para retornarse como JSON
+    st.session_state.datos_cargados = True
 
-# Parámetros de la solicitud
-params = dict(service='WFS',
-              version='2.0.0', 
-              request='GetFeature', 
-              typeName='IGN_5:limitecantonal_5k',
-              srsName='urn:ogc:def:crs:EPSG::5367',
-              outputFormat='json')
-
-# Solicitud
-response = requests.Request("GET", "https://geos.snitcr.go.cr/be/IGN_5/wfs", params=params).prepare().url
-
-# Leer datos del URL
-cantones = gpd.read_file(response)
-
-# Seleccionar únicamente las columnas necesarias para el análisis
-cantones = cantones[["canton", "geometry"]].copy()
-
-# Se calcula el área de cada cantón
-cantones['area_km2'] = cantones.area / 1000000 # se divide para convertirlo a km2
+    # Barra para mostrar el progreso de la carga de datos
+    my_bar = st.progress(0)
 
 
-#
-# Capa Red vial 1:200000
-#
+    #
+    # Capa Límite cantonal 1:5000
+    #
 
-# Solicitud de capa WFS de la red vial mediante GET, para retornarse como JSON
+    # Solicitud de capa WFS del límite cantonal mediante GET, para retornarse como JSON
 
-# Parámetros de la solicitud
-params = dict(service='WFS',
-              version='2.0.0', 
-              request='GetFeature', 
-              typeName='IGN_200:redvial_200k',
-              srsName='urn:ogc:def:crs:EPSG::5367',
-              outputFormat='json')
+    # Parámetros de la solicitud
+    params = dict(service='WFS',
+                version='2.0.0', 
+                request='GetFeature', 
+                typeName='IGN_5:limitecantonal_5k',
+                srsName='urn:ogc:def:crs:EPSG::5367',
+                outputFormat='json')
 
-# Solicitud
-response = requests.Request("GET", "https://geos.snitcr.go.cr/be/IGN_200/wfs", params=params).prepare().url
+    # Solicitud
+    response = requests.Request("GET", "https://geos.snitcr.go.cr/be/IGN_5/wfs", params=params).prepare().url
 
-# Leer datos del URL
-red_vial = gpd.read_file(response)
+    # Se actualiza la barra de progreso
+    my_bar.progress(10)
 
-# Seleccionar únicamente las columnas necesarias para el análisis
-red_vial = red_vial[["categoria", "geometry"]].copy()
+    # Leer datos del URL y almacenarlos en Session State
+    st.session_state.cantones = gpd.read_file(response)
 
+    # Se actualiza la barra de progreso
+    my_bar.progress(30)
+
+    # Seleccionar únicamente las columnas necesarias para el análisis
+    st.session_state.cantones = st.session_state.cantones[["canton", "geometry"]].copy()
+
+    # Se calcula el área de cada cantón
+    st.session_state.cantones['area_km2'] = st.session_state.cantones.area / 1000000 # se divide para convertirlo a km2
+
+    # Se actualiza la barra de progreso
+    my_bar.progress(50)
+
+
+    #
+    # Capa Red vial 1:200000
+    #
+
+    # Solicitud de capa WFS de la red vial mediante GET, para retornarse como JSON
+
+    # Parámetros de la solicitud
+    params = dict(service='WFS',
+                version='2.0.0', 
+                request='GetFeature', 
+                typeName='IGN_200:redvial_200k',
+                srsName='urn:ogc:def:crs:EPSG::5367',
+                outputFormat='json')
+
+    # Solicitud
+    response = requests.Request("GET", "https://geos.snitcr.go.cr/be/IGN_200/wfs", params=params).prepare().url
+
+    # Se actualiza la barra de progreso
+    my_bar.progress(60)
+
+    # Leer datos del URL y almacenarlos en Session State
+    st.session_state.red_vial = gpd.read_file(response)
+
+    # Se actualiza la barra de progreso
+    my_bar.progress(75)
+
+    # Seleccionar únicamente las columnas necesarias para el análisis
+    st.session_state.red_vial = st.session_state.red_vial[["categoria", "geometry"]].copy()
+
+    # Se actualiza la barra de progreso
+    my_bar.progress(100)
+    
 
 #
 # Especificación del filtro
 #
 
-# Categoría
-lista_categorias = red_vial.categoria.unique().tolist()
+# Categoría de la red vial
+lista_categorias = st.session_state.red_vial.categoria.unique().tolist()
 lista_categorias.sort()
-filtro_categoria = st.sidebar.selectbox('Seleccione la categoría de la red vial', lista_categorias)
+filtro_categoria = st.sidebar.selectbox('Seleccione la categoría de la red vial:', lista_categorias)
 
 
 
@@ -113,10 +138,10 @@ filtro_categoria = st.sidebar.selectbox('Seleccione la categoría de la red vial
 ##################
 
 # Filtrado
-red_vial = red_vial[red_vial['categoria'] == filtro_categoria]
+red_vial = st.session_state.red_vial[st.session_state.red_vial['categoria'] == filtro_categoria]
 
 # Intersección entre la capa de cantones y la capa de registros de la red vial
-cantones_red_vial = cantones.overlay(red_vial, how='intersection', keep_geom_type=False)
+cantones_red_vial = st.session_state.cantones.overlay(red_vial, how='intersection', keep_geom_type=False)
 
 # Se calcula la longitud de cada red vial. Se divide entre mil para convertirlo a km
 cantones_red_vial["longitud_km"] = cantones_red_vial.length / 1000
@@ -128,7 +153,7 @@ cantones_red_vial = cantones_red_vial.groupby(["canton"]).longitud_km.sum()
 cantones_red_vial = cantones_red_vial.reset_index() 
 
 # Join para agregar la columna de la longitud de la red vial
-cantones_red_vial = cantones.join(cantones_red_vial.set_index('canton'), on='canton')
+cantones_red_vial = st.session_state.cantones.join(cantones_red_vial.set_index('canton'), on='canton')
 
 # Se coloca en 0 la longitud de la red vial en los cantones que tienen nulo
 cantones_red_vial["longitud_km"] = cantones_red_vial.longitud_km.fillna(0)
@@ -193,7 +218,7 @@ with col2:
     cantones_red_vial_grafico = cantones_red_vial.sort_values("longitud_km", ascending=[False]).head(15)[["canton", "longitud_km"]]
 
     # Se obtiene la suma de la longitud de la red vial de los 67 cantones restantes 
-    suma_restantes = cantones_red_vial.sort_values("longitud_km", ascending=[True]).head(len(cantones) - 15).longitud_km.sum()
+    suma_restantes = cantones_red_vial.sort_values("longitud_km", ascending=[True]).head(len(st.session_state.cantones) - 15).longitud_km.sum()
 
     # Se agrega el registro de los otros cantones al Dataframe
     cantones_red_vial_grafico.loc[-1] = ["Otros cantones", suma_restantes]
@@ -227,7 +252,7 @@ m = folium.Map(location=[9.8, -84.2],
 # Se añade al mapa la capa de coropletas
 folium.Choropleth(
     name="Densidad de la red vial",
-    geo_data=cantones,
+    geo_data=st.session_state.cantones,
     data=cantones_red_vial,
     columns=['canton', 'densidad_vial'],
     bins=8,
